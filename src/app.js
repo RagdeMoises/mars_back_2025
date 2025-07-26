dd53f5e40742f316d06865112882191dde619f23
RagdeMoises <101891848+RagdeMoises@users.noreply.github.com>
2025-07-26 19:39:41 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Middleware
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: 'https://upload2025-production.up.railway.app'
+}));
 app.use(express.json());
 app.use("/images", express.static('public/uploads'));
 app.use(bodyParser.json({ limit: '50mb' }));
